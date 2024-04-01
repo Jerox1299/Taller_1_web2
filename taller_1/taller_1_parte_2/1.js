8e87b32c7e3778f1d100d14556d4b8da4ace6282
Jerónimo Londoño Cárdenas <130516046+Jerox1299@users.noreply.github.com>
2024-04-01 10:55:23 -0500

    Create 1.js

---
+++ taller_1/taller_1_parte_2/1.js
@@ -0,0 +1,12 @@
+// Cuadrado lleno
+
+//https://github.com/makeitrealcamp/ejercicios-javascript/blob/main/README.md
+const size = 5;
+
+for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+        row += "*";
+    }
+    console.log(row); 
+}
